015cff71efa345275b22408276567c65024858ac
Kelaven <kevin.lavenant@live.fr>
2024-09-16 21:21:37 +0200

    feat(content): style added

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,5 +1,16 @@
+import styles from "./Content.module.scss";
+
 function Content() {
-  return <div className="b2 flex-fill">Content</div>;
+  return (
+    <div className="container flex-fill">
+      <h1 className="mt-20 mb-20">Découvrez nos nouvelles recettes</h1>
+      <div className={`card p-20 ${styles.contentCard}`}>
+        <div className={styles.grid}>
+          <div className={styles.elem}>ELEMENTS</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Content;
