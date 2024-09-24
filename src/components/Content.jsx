eb9077ee60402a89957e48401df38adc1174d2fb
Kelaven <kevin.lavenant@live.fr>
2024-09-24 12:12:43 +0200

    feat(searchbar): feature added

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,18 +1,40 @@
 import styles from "./Content.module.scss";
 import Recipe from "./Recipe";
 import { data } from "../data/recipes";
+import { useState } from "react";
 
 function Content() {
+  const [filter, setFilter] = useState("");
   const recipes = data;
-  console.log(recipes);
+
+  function handleInput(e) {
+    const filter = e.target.value;
+    setFilter(filter.trim().toLowerCase());
+  }
+
   return (
     <div className="container flex-fill">
       <h1 className="mt-20 mb-20">Découvrez nos nouvelles recettes</h1>
-      <div className={`card p-20 ${styles.contentCard}`}>
+      <div className={`card d-flex flex-column p-20 ${styles.contentCard}`}>
+        <div
+          className={`d-flex flex-row justify-content-center align-item-center my-30 ${styles.searchBar}`}
+        >
+          <i className="fa-solid fa-magnifying-glass mr-15"></i>
+          <input
+            onInput={handleInput}
+            className="flex-fill"
+            type="text"
+            placeholder="Rechercher"
+          />
+        </div>
         <div className={styles.grid}>
-          {recipes.map((recipe, index) => (
-            <Recipe key={index} title={recipe.title} img={recipe.img} />
-          ))}
+          {recipes
+            .filter((recipe) =>
+              recipe.title.toLocaleLowerCase().startsWith(filter)
+            )
+            .map((recipe) => (
+              <Recipe key={recipe._id} title={recipe.title} img={recipe.img} />
+            ))}
         </div>
       </div>
     </div>
