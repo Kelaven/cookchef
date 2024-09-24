3eb19df436b0ee77d145ecc1180604bff7194374
Kelaven <kevin.lavenant@live.fr>
2024-09-24 11:56:37 +0200

    feat(menu btn): feature added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,33 @@
 import styles from "./Header.module.scss";
 import cookchefLogo from "../assets/images/logo-cookchef.png";
+import { useState } from "react";
+import HeaderMenu from "./HeaderMenu";
 
 function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <header className={`${styles.header} d-flex flex-row align-items-center`}>
-      <i className="fa-solid fa-bars"></i>
       <div className="flex-fill">
         <img src={cookchefLogo} alt="logo cookchef" />
       </div>
-      <ul>
+      <ul className={styles.headerList}>
         <button className="mr-5 btn btn-primary-reverse">
-          <i className="fa-solid fa-cart-shopping mr-5"></i>
-          <span>panier</span>
+          <i className="fa-solid fa-heart mr-5"></i>
+          <span>Wishlist</span>
         </button>
         <button className="btn btn-primary">connexion</button>
       </ul>
+      <i
+        className={`fa-solid fa-bars ${styles.headerXs}`}
+        onClick={() => setShowMenu(!showMenu)}
+      ></i>
+      {showMenu && (
+        <>
+          <div className="calc" onClick={() => setShowMenu(false)}></div>
+          <HeaderMenu />
+        </>
+      )}
     </header>
   );
 }
