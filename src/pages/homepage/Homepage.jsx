4e6617aabdaa01bcce042884b636226bcee968fc
Kelaven <kevin.lavenant@live.fr>
2024-09-26 20:27:28 +0200

    feat(homepage): pagination added

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -8,30 +8,40 @@ function Homepage() {
   const [filter, setFilter] = useState("");
   const [recipes, setRecipes] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
   const BASE_URL_API = useContext(ApiContext);
 
-  useEffect(() => {
-    let cancel = false;
-    async function fetchRecipes() {
-      try {
-        setIsLoading(true);
-        const response = await fetch(BASE_URL_API);
-        if (response.ok && !cancel) {
-          // cancel est initialisé à false. Donc au début, !cancel est équivalent à true, ce qui permet au code à l’intérieur du bloc de s’exécuter. Si le composant est démonté, cancel devient true, et !cancel deviendra false, bloquant l’exécution de cette partie du code.
-          const recipes = await response.json();
-          setRecipes(Array.isArray(recipes) ? recipes : [recipes]);
-        }
-      } catch (error) {
-        console.log(error);
-      } finally {
-        if (!cancel) {
-          setIsLoading(false);
+  useEffect(
+    () => {
+      let cancel = false;
+      async function fetchRecipes() {
+        try {
+          setIsLoading(true);
+          const response = await fetch(
+            `${BASE_URL_API}?skip=${(page - 1) * 18}&limit=18`
+          ); // page -1 car au début on skip zéro articles, ensuite on skipera les 18 premiers articles, puis les 36 premiers etc
+          if (response.ok && !cancel) {
+            // cancel est initialisé à false. Donc au début, !cancel est équivalent à true, ce qui permet au code à l’intérieur du bloc de s’exécuter. Si le composant est démonté, cancel devient true, et !cancel deviendra false, bloquant l’exécution de cette partie du code.
+            const newRecipes = await response.json();
+            setRecipes((x) =>
+              Array.isArray(recipes)
+                ? [...x, ...newRecipes]
+                : [...x, newRecipes]
+            );
+          }
+        } catch (error) {
+          console.log(error);
+        } finally {
+          if (!cancel) {
+            setIsLoading(false);
+          }
         }
       }
-    }
-    fetchRecipes();
-    return () => (cancel = true); // clean up. La variable est définie à false au départ. Si le composant est démonté, la fonction de nettoyage (return) modifie cancel à true, et ainsi, si la requête est toujours en cours, les états ne seront pas mis à jour, évitant des erreurs ou des comportements inattendus.
-  }, [BASE_URL_API]);
+      fetchRecipes();
+      return () => (cancel = true); // clean up. La variable est définie à false au départ. Si le composant est démonté, la fonction de nettoyage (return) modifie cancel à true, et ainsi, si la requête est toujours en cours, les états ne seront pas mis à jour, évitant des erreurs ou des comportements inattendus.
+    },
+    [BASE_URL_API, page] // NB : penser à ajouter page aux dépendances car il changera et devra rafraîchir l'affichage
+  );
 
   function updateRecipe(updatedRecipe) {
     setRecipes(
@@ -63,7 +73,7 @@ function Homepage() {
             placeholder="Rechercher"
           />
         </div>
-        {isLoading ? (
+        {isLoading && !recipes.length ? (
           <Loading />
         ) : (
           <div className={styles.grid}>
@@ -84,6 +94,11 @@ function Homepage() {
               ))}
           </div>
         )}
+        <div className="d-flex flex-row justify-content-center align-items-center p-20">
+          <button onClick={() => setPage(page + 1)} className="btn btn-primary">
+            Charger plus de recettes
+          </button>
+        </div>
       </div>
     </div>
   );
